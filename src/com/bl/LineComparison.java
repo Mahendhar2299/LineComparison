234d7c94edf52b71ac2bc12be22dc88b8d794ac2
Mahendhar2299 <delphic22mahi@gmail.com>
2023-06-04 23:01:46 +0530

    Added: Code for Length of the line

--- src/com/bl/LineComparison.java
+++ src/com/bl/LineComparison.java
@@ -1,7 +1,16 @@
 package com.bl;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the values of x1, y1, x2, y2: ");
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+        double lengthOfLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Length of the line is = " + lengthOfLine);
     }
 }
